742997e7e09d60454627b823f1e89f8e7b3e0b39
JonathanDieke <louloulexus@gmail.com>
2021-02-19 18:29:06 +0000

    finish him

--- app/Models/Godchild.php
+++ app/Models/Godchild.php
@@ -10,7 +10,7 @@ class Godchild extends Model
 {
     use HasFactory;
 
-    // protected $primaryKey = 'register';
+    protected $primaryKey = 'register';
     // protected $primaryKey = null;
     // public $incrementing = false;
 
