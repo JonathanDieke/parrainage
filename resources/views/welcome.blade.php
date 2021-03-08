ba0def399fd971e1783109634b4b94a9246876c7
JonathanDieke <louloulexus@gmail.com>
2021-03-08 20:24:16 +0000

    replace lorem with antitode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,30 @@
     </div>
 
     <div class="description mx-auto text-center ">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+
+
+
+        <figure class="text-center p-3">
+            <blockquote class="blockquote">
+              <p>
+                Si tu donnes tout pour y arriver, attends toi à tout perdre quand t’ y seras.
+                La Vie c’est transformer quelque chose d’ordinaire en quelque chose d’extraordinaire jusqu’à ce qu’elle devienne ordinaire à nouveau.
+                Ne crois pas en tes rêves, réalise les. Meurs en ayant tout donné car tu n’ emporteras rien avec toi.
+                Fais ce qu’il te plaît jusqu’à ce que ça plaise aux autres et pas le contraire...
+                </p>
+            </blockquote>
+            <figcaption class="blockquote-footer p-2">
+                Damso, <cite title="Source Title"> L'Antidote</cite>
+            </figcaption>
+          </figure>
+
+        {{--  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
         Ipsum totam error sint distinctio sapiente, officia hic, possimus aspernatur, expedita facilis dignissimos rerum ipsam?
         Obcaecati ea facere animi, cumque laudantium nam sapiente possimus delectus sint fugit illo magnam tempore asperiores
         aspernatur voluptas explicabo et? Repellat temporibus perferendis, enim consequuntur doloremque error?
         <br>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut porro architecto, magni aperiam illo placeat!
-        Consequuntur quae tenetur, voluptates nisi rerum animi illum sit laborum vitae aliquid aperiam autem deserunt.
+        Consequuntur quae tenetur, voluptates nisi rerum animi illum sit laborum vitae aliquid aperiam autem deserunt.  --}}
     </div>
 
     <div class="buttons d-flex justify-content-center mt-4" style="width: 100%">
